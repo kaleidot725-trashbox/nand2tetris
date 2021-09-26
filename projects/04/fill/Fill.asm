e60f54d0a5b833893eeebf5b556beb86a92fde4a
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-09-26 13:18:15 +0900

    feat: add fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,48 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+(INIT)
+    // カウンタ初期化
+    @SCREEN
+    D=A
+    @0
+    M=D
+    // 行数カウンタ
+    @8192
+    D=A
+    @1
+    M=D
+(KBD_CHECK)
+    @KBD
+    D=M
+    @FILL_LOOP
+    D;JNE
+    @UNFILL_LOOP
+    D;JEQ
+(FILL_LOOP)
+    // 黒くする作業
+    @0
+    A=M
+    M=-1
+    D=A+1
+    @0
+    M=D
+    // 行数カウンタを減らす
+    @1
+    MD=M-1
+    @FILL_LOOP
+    D;JNE
+    @INIT
+    0;JMP
+(UNFILL_LOOP)
+    // 白くする作業
+    @0
+    A=M
+    M=0
+    D=A+1
+    @0
+    M=D
+    // 行数カウンタを減らす
+    @1
+    MD=M-1
+    @UNFILL_LOOP
+    D;JNE
+    @INIT
+    0;JMP
